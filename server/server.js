521803b167f5c4bcf126e2b1b8fde5da582032c3
Andrew Betts <andrew@trib.tv>
2023-01-04 10:15:11 +0000

    Simlify + logging

--- server/server.js
+++ server/server.js
@@ -15,13 +15,14 @@ process.on('uncaughtException', (err) => console.log('Unhandled exception: ' + e
 process.on('unhandledRejection', (err) => console.log('Unhandled promise rejection: ' + err));
 process.on('warning', (err) => console.log('Process warning: ' + err));
 
-// If the request does not contain a content type, make it text/plain
 app.use((req, resp, next) => {
-  if (req.method === 'POST') {
-    req.headers['content-type'] = req.headers['content-type'] || 'text/plain';
+  // If the request does not contain a content type, assume it is text/plain
+  if (req.method === 'POST' && !req.headers['content-type']) {
+    req.headers['content-type'] = 'text/plain';
   }
   resp.set({
-    "Access-Control-Allow-Origin": req.headers['origin'] || '*'
+    'Access-Control-Allow-Origin': req.headers['origin'] || '*',
+		'X-Accel-Buffering': 'no' // Disables response buffering on Google App Engine (flex env)
   });
   next();
 });
@@ -33,27 +34,16 @@ app.set('views', './views');
 app.disable('x-powered-by');
 app.disable('etag');
 
-app.use((req, res, next) => {
-  res.set({
-    'Content-Security-Policy': "default-src 'self'; img-src 'self' cdn.glitch.com",
-    'X-Frame-Options': "SAMEORIGIN",
-    'X-XSS-Protection': "1",
-    'X-Content-Type-Options': "nosniff",
-    'Referer-Policy': "origin-when-cross-origin",
-		'Strict-Transport-Security': "max-age=86400",
-		'X-Accel-Buffering': 'no' // Disables response buffering on Google App Engine (flex env)
-  });
-  next();
-})
-
 // Allow this tool to be a log destination for Fastly's real time logging feature
 app.all('/.well-known/fastly/logging/challenge', (req, res) => res.end("*"));
 
 // Provide a healthcheck endpoint for Google App Engine
 app.get('/__health', (req, res) => res.end('OK'));
 
+// Redirect the root path to a random bucket
 app.get('/', (req, res) => res.redirect('/' + hri.random()));
 
+// Render a viewer UI or deliver an event stream for a bucket
 app.get('/:bucketID', (req, res) => {
   if (req.params.bucketID.length < MIN_BUCKET_ID_LENGTH) {
     res.status(404);
@@ -67,7 +57,7 @@ app.get('/:bucketID', (req, res) => {
       res.status(429);
       res.end('This stream already has the maximum number of clients connected');
     } else {
-      console.log('New subscriber for ' + req.params.bucketID);
+      console.log({ msg: "New subscriber", bucket: req.params.bucketID })
       ch.subscribe(req, res);
       const clients = Object.keys(ch.listClients());
       const connCount = ch.getSubscriberCount();
@@ -78,21 +68,28 @@ app.get('/:bucketID', (req, res) => {
   }
 });
 
+// Process new events
 app.post('/:bucketID', (req, res) => {
-  console.log(req.path, req.headers);
-  req.body && req.body.split(/\n+/).filter(x => x && x.length).forEach(line => {
-    const evt = new ParsedEvent(line);
-    evt.parse();
+  const lines = req.body && req.body.split(/\n+/).filter(x => x && x.length)
+  if (lines) {
+    console.log({ msg: "New events", bucket: req.params.bucketID, eventCount: lines.length })
+    lines.forEach(line => {
+      const evt = new ParsedEvent(line);
+      evt.parse();
 
-    channelManager.getChannel(req.params.bucketID).publish({
-      time: evt.getTime(),
-      raw: line,
-      fields: evt.getFields(),
-      parser: evt.getParserName()
-    }, 'log');
+      channelManager.getChannel(req.params.bucketID).publish({
+        time: evt.getTime(),
+        raw: line,
+        fields: evt.getFields(),
+        parser: evt.getParserName()
+      }, 'log');
 
-  });
-  res.status(204);
+    });
+    res.status(204);
+  } else {
+    res.status(400);
+    res.end("No content received")
+  }
   res.end();
 });
 
