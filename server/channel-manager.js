4464bc1b27c71de8044f42629c98683d7ab4dd17
Andrew Betts <andrew@trib.tv>
2023-01-10 12:53:53 +0000

    Avoid destroying channels during client reconnects

--- server/channel-manager.js
+++ server/channel-manager.js
@@ -1,7 +1,8 @@
 const SSEChannel = require('sse-pubsub');
 
 const SSE_OPTIONS = { historySize: 10 };
-const GC_INTERVAL = 5000;
+const GC_INTERVAL = 60000;
+const GC_WAIT = 10000;
 
 const channels = new Map();
 
@@ -15,7 +16,14 @@ module.exports.getChannel = name => {
 setInterval(() => {
   for (let [name, channel] of channels) {
     if (channel.getSubscriberCount() === 0) {
-      channels.delete(name);
+
+      // Only delete it if it continues to have no subscribers for a short period
+      // (allowing for client reconnects)
+      setTimeout(() => {
+        if (channel.getSubscriberCount() === 0) {
+          channels.delete(name);
+        }
+      }, GC_WAIT);
     }
   }
 }, GC_INTERVAL);
